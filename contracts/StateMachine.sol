29c2238fad4acbd0dfc450ec3f2f9685c30bae71
tighug <kapuriko123@gmail.com>
2019-12-09 03:56:34 +0900

    [add] StateMachine.solの追加

---
+++ contracts/StateMachine.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.5.13;
+
+contract StateMachine {
+    enum Stages {AcceptingBids, AcceptingAuction, WaitingAuction, Finished}
+
+    Stages public stage = Stages.AcceptingBids;
+
+    uint256 public creationTime = now;
+    uint256 public bidPeriod;
+
+    modifier atStage(Stages _stage) {
+        require(stage == _stage, "Function cannot be called at this time.");
+        _;
+    }
+
+    modifier timedTransitions() {
+        if (stage == Stages.AcceptingBids && now >= creationTime + bidPeriod)
+            _nextStage();
+        _;
+    }
+
+    constructor(uint256 _bidPeriod) public {
+        bidPeriod = _bidPeriod;
+    }
+
+    function _nextStage() internal {
+        stage = Stages(uint256(stage) + 1);
+    }
+}
