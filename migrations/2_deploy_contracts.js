a844ef808395072d16a12fbf747cb6a6b30fa918
tighug <kapuriko123@gmail.com>
2019-12-16 08:36:34 +0900

    [rename] ファイル名の変更・修正

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,25 +2,33 @@
 const ElectricityMarket = artifacts.require("./ElectricityMarket.sol");
 // eslint-disable-next-line no-undef
 const ENGToken = artifacts.require("./ENGToken.sol");
+require("@openzeppelin/test-helpers/configure")({
+  // eslint-disable-next-line no-undef
+  provider: web3.currentProvider,
+  environment: "truffle"
+});
+const {singletons} = require("@openzeppelin/test-helpers");
 const fs = require("fs");
 
 module.exports = function(deployer, network, accounts) {
   deployer.then(async () => {
-    // await deployer
-    //   .deploy(ENGToken, [accounts[0]], {
-    //     from: accounts[0],
-    //     gas: 6721975
-    //   })
-    //   .then(() => {
-    //     try {
-    //       fs.writeFileSync("token", ENGToken.address);
-    //       console.log("write end");
-    //     } catch (e) {
-    //       console.log(e);
-    //     }
-    //   });
+    await singletons.ERC1820Registry(accounts[0]);
+
     await deployer
-      .deploy(ElectricityMarket, {
+      .deploy(ENGToken, [accounts[0]], {
+        from: accounts[0],
+        gas: 6721975
+      })
+      .then(() => {
+        try {
+          fs.writeFileSync("token", ENGToken.address);
+          console.log("write end");
+        } catch (e) {
+          console.log(e);
+        }
+      });
+    await deployer
+      .deploy(ElectricityMarket, ENGToken.address, 100, {
         from: accounts[0],
         gas: 6721975,
         value: 50000000000000000
