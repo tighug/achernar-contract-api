a5fbb1d9fb97571507064b670b802a575dc9c7cc
tighug <kapuriko123@gmail.com>
2020-01-06 07:55:20 +0900

    [clean] lint

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,23 +1,25 @@
 pragma solidity ^0.5.0;
 
+
 contract Migrations {
     address public owner;
-    uint256 public last_completed_migration;
+    uint256 public lastCompletedMigration;
 
     constructor() public {
         owner = msg.sender;
     }
 
     modifier restricted() {
-        if (msg.sender == owner) _;
+        if (msg.sender == owner)
+            _;
     }
 
     function setCompleted(uint256 completed) public restricted {
-        last_completed_migration = completed;
+        lastCompletedMigration = completed;
     }
 
-    function upgrade(address new_address) public restricted {
-        Migrations upgraded = Migrations(new_address);
-        upgraded.setCompleted(last_completed_migration);
+    function upgrade(address newAddress) public restricted {
+        Migrations upgraded = Migrations(newAddress);
+        upgraded.setCompleted(lastCompletedMigration);
     }
 }
