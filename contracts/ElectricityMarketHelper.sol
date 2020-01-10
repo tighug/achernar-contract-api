e915804f88eb8dd838b418584eb45d409f8c4006
tighug <kapuriko123@gmail.com>
2020-01-10 13:36:03 +0900

    [Clean] lint

--- contracts/ElectricityMarketHelper.sol
+++ contracts/ElectricityMarketHelper.sol
@@ -6,55 +6,54 @@ import "./ElectricityMarketModel.sol";
 
 
 contract ElectricityMarketHelper is usingProvable, ElectricityMarketModel {
-  using strings for *;
+    using strings for *;
 
-  function createBidJson(Bid[] memory bids)
-    internal
-    pure
-    returns (string memory)
-  {
-    string memory s_bids;
+    function createBidJson(Bid[] memory bids)
+        internal
+        pure
+        returns (string memory)
+    {
+        string memory strBids;
 
-    for(uint256 i = 0; i < bids.length; i++) {
-      Bid memory bid = bids[i];
-      string memory s_bidType;
-      if(bid.bidType == BidTypes.Buy) {
-        s_bidType = "b";
-      }else {
-        s_bidType = "s";
-      }
-      
-      string memory s_index = uint2str(i);
-      string memory s_price = uint2str(bid.price);
-      string memory s_amount = uint2str(bid.amount);
-      string memory s_nodeNum = uint2str(bid.nodeNum);
-      string memory concatedIndex = strConcat("{ index: ", s_index, ",");
-      string memory concatedBidType = strConcat("bidType: ", s_bidType, ",");
-      string memory concatedPrice = strConcat("price: ", s_price, ",");
-      string memory concatedAmount = strConcat("amount: ", s_amount, ",");
-      string memory concatedNodeNum = strConcat("nodeNum: ", s_nodeNum, "}");
-      string memory concated = strConcat(
-        concatedIndex,
-        concatedBidType,
-        concatedPrice,
-        concatedAmount,
-        concatedNodeNum
-      );
+        for (uint256 i = 0; i < bids.length; i++) {
+            Bid memory bid = bids[i];
+            string memory strBidType;
 
-      s_bids = strConcat(s_bids, concated);
+            if (bid.bidType == BidTypes.Buy) {
+                strBidType = "b";
+            } else {
+                strBidType = "s";
+            }
 
-      if(i != bids.length - 1) {
-        s_bids = strConcat(s_bids, ",");
-      }
+            string memory strIndex = uint2str(i);
+            string memory strPrice = uint2str(bid.price);
+            string memory strAmount = uint2str(bid.amount);
+            string memory strNodeNum = uint2str(bid.nodeNum);
+            string memory concatedIndex = strConcat("{ index: ", strIndex, ",");
+            string memory concatedBidType = strConcat("bidType: ", strBidType, ",");
+            string memory concatedPrice = strConcat("price: ", strPrice, ",");
+            string memory concatedAmount = strConcat("amount: ", strAmount, ",");
+            string memory concatedNodeNum = strConcat("nodeNum: ", strNodeNum, "}");
+            string memory concated = strConcat(
+                concatedIndex,
+                concatedBidType,
+                concatedPrice,
+                concatedAmount,
+                concatedNodeNum
+            );
+
+            strBids = strConcat(strBids, concated);
+
+            if (i != bids.length - 1) {
+                strBids = strConcat(strBids, ",");
+            }
+        }
+
+        return strConcat("{ bids: [", strBids, "] }");
     }
 
-    return strConcat("{ bids: [", s_bids, "] }");
-  }
-
-  function createBidAmmounts(string memory auctionJson)
-    internal
-    returns (uint[] memory)
-  {
-
-  }
+    function createBidAmmounts(string memory auctionJson)
+        internal
+        returns (uint[] memory)
+    { }
 }
