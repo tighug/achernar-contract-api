86ccd116963a7fd88d4ac1e3cb6240f09f50b09a
tighug <kapuriko123@gmail.com>
2020-01-10 13:36:28 +0900

    [Clean] lint

--- contracts/ElectricityMarketModel.sol
+++ contracts/ElectricityMarketModel.sol
@@ -1,13 +1,14 @@
 pragma solidity 0.5.14;
 
-contract ElectricityMarketModel {
-  enum BidTypes {Buy, Sell}
 
-  struct Bid {
-      BidTypes bidType;
-      uint256 price;
-      uint256 amount;
-      uint256 nodeNum;
-      bool didBid;
-  }
+contract ElectricityMarketModel {
+    enum BidTypes {Buy, Sell}
+
+    struct Bid {
+        BidTypes bidType;
+        uint256 price;
+        uint256 amount;
+        uint256 nodeNum;
+        bool didBid;
+    }
 }
