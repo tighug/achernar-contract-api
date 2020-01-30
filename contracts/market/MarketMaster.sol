aaefc1b5c7ced1bc4181b6f2b23fb0446e79eaf3
tighug <kapuriko123@gmail.com>
2020-01-30 13:43:04 +0900

    [Update] MarketMaster.solを修正

--- contracts/market/MarketMaster.sol
+++ contracts/market/MarketMaster.sol
@@ -5,20 +5,28 @@ import "./Market.sol";
 
 
 contract MarketMaster is Ownable {
-    Market[] private _markets;
+    Market[] markets;
 
     function createMarket(
-        address owner,
-        string calldata name,
-        uint256 bidPeriod
-    ) external onlyOwner returns (Market market)
+        address _owner,
+        address _userMaster,
+        address _elecMaster,
+        string calldata _name,
+        uint256 _feederId,
+        uint256 _bidPeriod,
+        string calldata _auctionApi
+    )
+        external onlyOwner returns (Market market)
     {
-        market = new Market(name, bidPeriod);
-        market.transferOwnership(owner);
-        _markets.push(market);
-    }
-
-    function markets() public view returns (Market[] memory) {
-        return _markets;
+        market = new Market(
+            _userMaster,
+            _elecMaster,
+            _name,
+            _feederId,
+            _bidPeriod,
+            _auctionApi
+        );
+        market.transferOwnership(_owner);
+        markets.push(market);
     }
 }
